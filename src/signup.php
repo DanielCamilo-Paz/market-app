bed7c760809f70026ccf1dda83328d7c1c67462a
Daniel Paz <daneilpaz3020@gmail.com>
2025-09-18 11:29:37 -0500

    18/09/2025

--- src/signup.php
+++ src/signup.php
@@ -18,31 +18,31 @@
       FROM
          users u
       WHERE
-         email = '$e_mail' or idnumber =  '$id_number'
+         email = '$e_mail' or id_number =  '$id_number'
       LIMIT 1
    ";
-   $res_check = pg_query($com,$check_mail);
-   if(pg_num_rows($res_check)>0){
+   $res_check = pg_query($conn, $check_email);
+   if(pg_num_rows($res_check) > 0){
       echo "<script>alert('User already exists !!! Go to login')</script>";
-      header ('refresh:0;url= signin.html');
-   }else{
+      header('refresh:0;url= signin.html');
+      exit();
+   } else {
    //Step 3 create query to insert into
    $query = "INSERT INTO users(firstname,lastname,mobile_number,id_number,email,password)
-   Values('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
+   VALUES('$f_name','$l_name','$m_number','$id_number','$e_mail','$enc_pass')";
 
    //Step 4  execute query
-   $res= pg_query($conn,$query);
+   $res = pg_query($conn, $query);
 
    //Step 5 validate result
    if($res){
       //echo "User has been created succesfully!!!";  
       echo "<script>alert('Succes !!! Go to login')</script>";
-      header ('refresh:0;url= signin.html');
-
-   }else{
+      header('refresh:0;url= signin.html');
+      exit();
+   } else {
       echo "something wrong !";
    }
 
    }
-
 ?>
