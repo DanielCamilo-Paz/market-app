38ce3e097d0b471f6c2a996f7d9c39ab81dd6a75
Daniel Paz <daneilpaz3020@gmail.com>
2025-10-09 11:34:55 -0500

    Config supabase and local database connection in database file

--- config/database.php
+++ config/database.php
@@ -1,21 +1,37 @@
 <?php
-  //Data base connection
-  $host     =   "localhost"; //127.0.0.1
-  $user     =   "postgres";
-  $password =   "unicesmag";
-  $dbname   =   "marketapp";
-  $port     =   "5432";
+ //Data base connection to local server 
+    $supa_host     =   "aws-1-us-east-2.pooler.supabase.com"; 
+    $supa_user     =   "postgres.vvyzezupnfdlidcgumjv";
+    $supa_password =   "1081053020";
+    $supa_dbname   =   "postgres";
+    $supa_port     =   "6543";
 
-  $data_connection = "
-  host= $host
-  user= $user
-  password= $password
-  dbname= $dbname
-  port= $port
+  //Data base connection to local server 
+    $local_host     =   "localhost"; //127.0.0.1
+    $local_user     =   "postgres";
+    $local_password =   "unicesmag";
+    $local_dbname   =   "marketapp";
+    $local_port     =   "5432";
+
+  $supa_data_connection = "
+  host      =   $supa_host
+  user      =   $supa_user
+  password  =   $supa_password
+  dbname    =   $supa_dbname
+  port      =   $supa_port
+  ";
+$local_data_connection = "
+  host      =   $local_host
+  user      =   $local_user
+  password  =   $local_password
+  dbname    =   $local_dbname
+  port      =   $local_port
   ";
 
-$conn = pg_connect($data_connection);
-if(!$conn){
+$conn_supa  = pg_connect($supa_data_connection);
+$conn_local = pg_connect($local_data_connection);
+
+if(!$conn_supa){
     echo "Error";
 }else{
     echo "Connection successfully ::: ";
