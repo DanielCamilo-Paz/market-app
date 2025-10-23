a82ff6dbcaea7b0e2c21ac040b2f4d0862b206c9
Daniel Paz <daneilpaz3020@gmail.com>
2025-10-23 11:35:43 -0500

    print list users

--- config/database.php
+++ config/database.php
@@ -28,12 +28,18 @@ $local_data_connection = "
   port      =   $local_port
   ";
 
-$conn_supa  = pg_connect($supa_data_connection);
+//$conn_supa  = pg_connect($supa_data_connection);
 $conn_local = pg_connect($local_data_connection);
 
-//*if(!$conn_supa){
-//    echo "Error";
-//}else{
-//    echo "Connection successfully ::: ";
-//}
+if(!$conn_local){
+    echo "Error". preg_last_error();
+}else{
+    echo "Connection successfully ::: ";
+}
+/*
+if(!$conn_supa){
+    //echo "Error". preg_last_error();
+}else{
+   // echo "Connection successfully ::: ";
+}*/
 ?>
--- src/list_users.php
+++ src/list_users.php
@@ -20,18 +20,44 @@
      <th>Status</th>
      <th>Options</th>
     </tr> 
-    <?php  
-      <tr>
-         <td>Pepito Perez</td>
-        <td>pe@gmail.com</td>
-         <td>1234567</td>
-         <td>3014585</td>
-         <td>Active</td>
-         <td>
-         <a href = "#"> <img src = "icons/search.png" width = "30">  </a>
-         <a href = "#"> <img src = "icons/Update.png" width = "30"></a>
-         <a href = "#"> <img src = "icons/delete.png" width = "30"> </a>
-</td>
+    
+    <?php
+    $sql_users = " 
+    select 
+        u.firstname || ' '|| u.lastname as lastname,
+        u.email,
+        u.id_number,
+        u.mobile_number,   
+        case 
+	        when u.status = true then 'active' else 'inactive' 
+        end as status 
+    from 
+        users u     
+    ";
+      $result = pg_query($conn_local, $sql_users);
+
+      if(!$result){
+        die ("Erro: " .preg_last_error() );
+      }
+      
+      while ($row = pg_fetch_assoc($result)){
+        echo "<tr>
+                <td>".$row['lastname']. "</td>
+                <td>".$row['email']. "</td>
+                <td>".$row['id_number']. "</td>
+                <td>".$row['mobile_number']. "</td>
+                <td>Active</td>
+                <td>
+                    <a href = '#'> <img src = 'icons/search.png' width = '30'> </a>
+                    <a href = '#'> <img src = 'icons/Update.png' width = '30'></a>
+                    <a href = '#'> <img src = 'icons/delete.png' width = '30'> </a>
+                </td>
+            </tr>
+            ";
+      }
+    ?>
+      
+
 </table>
 </body>
 </html>
